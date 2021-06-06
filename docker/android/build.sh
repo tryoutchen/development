d2f64f98cc55ac4ffe9d22485e468e105e781c7e
Tryout Chen <tryout.chen@goldenrivertek.com>
2021-06-06 11:48:01 +0800

    add ubuntu-focal for aosp master branch

---
+++ docker/android/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eo pipefail; [[ "$TRACE" ]] && set -x
+
+if [ ! -f ~/.gitconfig ]; then
+  echo "Please git-config first."
+  exit 1
+fi
+
+cp ~/.gitconfig gitconfig
+
+docker build \
+    --build-arg username=$(id -un) \
+    --build-arg timezone=$(cat /etc/timezone) \
+    -t android-build-focal .
